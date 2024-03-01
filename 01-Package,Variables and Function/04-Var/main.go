956ad580212c0019ef16a619cc2762d55a5e709e
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2024-03-01 20:15:32 +0530

    Variable declaration

---
+++ 01-Package,Variables and Function/04-Var/main.go
@@ -0,0 +1,23 @@
+// package main
+
+// import "fmt"
+// var a ,b, c bool
+// func main(){
+// 	var x int = 10
+// 	fmt.Println("Hello World" , x, a,b,c)
+// }
+
+package main
+
+import "fmt"
+
+var i ,j  = 1,2
+
+func main(){
+	var c , python ,java = true, false ,false
+	z := 10
+	fmt.Println(i , j , c ,python , java ,z)
+}
+
+// := construct is only available inside the functions not in the root scope
+
