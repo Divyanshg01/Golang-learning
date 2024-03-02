e42631d22ea134cd2e3faa758c0708ed30db399f
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2024-03-02 15:42:55 +0530

    for as while

--- 02-FlowAndControl/01-Loops/main.go
+++ 02-FlowAndControl/01-Loops/main.go
@@ -4,9 +4,22 @@ import "fmt"
 
 func main(){
 
-	sum := 0
-	for i:=0 ; i<5 ;i++{
-		sum++;
+	sum := 1
+	// for i:=0 ; i<5 ;i++{
+	// 	sum++;
+	// }
+
+	//can act as a while like
+	// for ;sum<1000;{
+	// 	sum +=sum
+	// }
+	//we can rewrite it as 
+	for sum < 1000 {
+		sum += sum
 	}
+	// infinite loop
+	// for {
+
+	// }
 	fmt.Println(sum)
 }
